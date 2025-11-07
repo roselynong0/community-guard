3ffb25be9590505755cbfa43446c3f34aa6fd59c
Roselyn Lei Ong <202312260@gordoncollege.edu.ph>
2025-11-07 13:16:44 +0800

    Railway changed.

--- start.sh
+++ start.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-pip3 install -r requirements.txt
+# Railway Launch Script
+
 cd backend
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Run using Gunicorn + factory pattern
 gunicorn --bind 0.0.0.0:$PORT "app:create_app()"
