53e85f15c9800ed2df91d42ef4b312fae8d029ba
Roselyn Lei Ong <202312260@gordoncollege.edu.ph>
2025-11-09 14:14:56 +0800

    Improved Merging of Admin Layout.

--- start.sh
+++ start.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-# Railway Launch Script
-
 cd backend
-
-# Install dependencies
-pip install -r requirements.txt
-
-# Run using Gunicorn + factory pattern
 gunicorn --bind 0.0.0.0:$PORT "app:create_app()"
